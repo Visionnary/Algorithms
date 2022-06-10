292069a04e9e75b643c195d8423d83225e2ac637
Visionnary <103528164+Visionnary@users.noreply.github.com>
2022-06-10 16:46:42 -0400

    Update binarySearch.py

---
+++ Algorithms-main/Algorithms/binarySearch.py
@@ -0,0 +1,14 @@
+import dataGen
+
+
+def binarySearch(arr, l, r, x):
+ 
+    while l <= r:
+        mid = l + (r - l) // 2
+        if arr[mid].model == x:
+            return mid
+        elif arr[mid].model < x:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return "Not Found"
