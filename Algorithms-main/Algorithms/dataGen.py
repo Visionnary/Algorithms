f1347211967a1988b6c79b134ca05d87136bb864
Visionnary <103528164+Visionnary@users.noreply.github.com>
2022-06-11 12:29:47 -0400

    Update dataGen.py

--- Algorithms-main/Algorithms/dataGen.py
+++ Algorithms-main/Algorithms/dataGen.py
@@ -1,22 +1,26 @@
 import random
 import string
 
+
 class Droid:
- def __init__(self, code, model):
+    def __init__(self, code, model):
         self.code = code
         self.model = model
 
- def __str__(self):
-        return("(code: " + str(self.code) + ", " + "model: " + str(self.model) + ")")
+    def __str__(self):
+        return "(code: " + str(self.code) + ", " + "model: " + str(self.model) + ")"
 
- def __repr__(self):
-        return str(self)   
+    def __repr__(self):
+        return str(self)
+
 
 def generateData(size):
-     newList = []
-     for i in range(size):
-      code = ''.join (random.choice(string.ascii_uppercase) for i in range (10))
-      model = round(random.random()*size)
-      newDroid = Droid(code, model)
-      newList.append(newDroid)
-     return newList
+    newList = []
+    for i in range(size):
+        code = ''.join(random.choice(string.ascii_uppercase) for i in range(10))
+        model = round(random.random() * size)
+        newDroid = Droid(code, model)
+        newList.append(newDroid)
+    return newList
+
+
