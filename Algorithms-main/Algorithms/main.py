cf63b54a6413149658af330694b7be782feae465
Visionnary <103528164+Visionnary@users.noreply.github.com>
2022-06-11 12:28:44 -0400

    Update main.py

--- Algorithms-main/Algorithms/main.py
+++ Algorithms-main/Algorithms/main.py
@@ -1,30 +1,40 @@
-import time
 import random
-import copy
+import time
+from binarySearch import binarySearch
 from dataGen import generateData
-from dataGen import genI
 from insertionSort import insertionSort
 from linearSearch import linearSearch
-from binarySearch import binarySearch
-
-input = int(input())
-startTime = time.time()
-
-if __name__ == "__main__":
-    list = generateData(input)
-    x = random.choice(list).model
-    print(x)
-    print(list)
-    print(binarySearch(list, 0, len(list)-1, x))
-    #print(insertionSort(list))
-    #print(linearSearch(list, 5))
-    #list.sort(key = lambda y: y.model)
-endTime = time.time()
+''' Selectively remove specific hashtags to see run the trials you want'''
 
 
-class main():
-    count = 0
-    n = 1000
-    for i in range(n):
-        count = count + 2
-    print("This took " + str(endTime - startTime) + " s")
+
+timeArr = []
+input = [5, 10, 100, 1000, 10000, 30000, 50000, 75000]
+quantity = [5, 10, 100, 1000, 10000, 30000, 50000, 75000]
+for input in quantity:
+    list1 = generateData(input)
+    # insertionSort(list1)
+    # list.sort(key=lambda y: y.model)
+    
+    for j in range(1,11):
+        # x = random.choice(list1).model
+        # y = 10000000000
+        startTime = time.time()
+        # insertionSort(list1)
+        # list1.sort(key=lambda y: y.model)
+        # for i in range(0, 20):
+        #     print(list1[i])
+        # for j in range(len(list1) - 20, len(list1)):
+        #     print(list1[j])
+        # linearSearch(list1, y)
+        # binarySearch(list1, 0, len(list1) - 1, y)
+        # for i in range(0, 20):
+        #     print(list1[i])
+        # for j in range(len(list1) - 20, len(list1)):
+        #     print(list1[j])
+        endTime = time.time()
+        random.shuffle(list1)
+        diff = endTime - startTime
+        timeArr.append(diff)
+
+print(timeArr)
